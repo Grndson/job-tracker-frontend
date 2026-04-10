b85a7977ac15158b82edac3aae59769f8cda8a64
Papafrost <vukoedmund00@gmail.com>
2026-04-10 11:54:45 +0300

    fix: Root URL

--- src/main.jsx
+++ src/main.jsx
@@ -44,6 +44,7 @@ createRoot(document.getElementById('root')).render(
       />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login"    element={<Login />} />
           <Route path="/register" element={<Register />} />
           
